8d24b892550276819f4f0412726d5ea08e01c68b
woraput <woraput@kkumail.com>
2018-11-03 15:10:52 +0700

    add more condition in InputAmountAndPayReturnChangeAndBank

--- HackChange.test/HackChangeTest.cs
+++ HackChange.test/HackChangeTest.cs
@@ -6,17 +6,17 @@ namespace HackChange.test
     public class HackChangeTest
     {
         [Theory]
-        [InlineData(0, new int[] { 0, 0, 0, 0, 0, 0, 0 }, 1000, 1000)]
-        [InlineData(648, new int[] { 0, 1, 1, 0, 2, 1, 3 }, 352, 1000)]
-        [InlineData(350, new int[] { 0, 0, 3, 1, 0, 0, 0 }, 650, 1000)]
-        
-        public void InputAmountAndPayReturnChangeAndBank(int expectedChange, int[] expectedBankCards, int amount, int pay)
+        [InlineData(0, 0, new int[] { 0, 0, 0, 0, 0, 0, 0 }, 1000, 1000)]
+        [InlineData(0, 648, new int[] { 0, 1, 1, 0, 2, 1, 3 }, 352, 1000)]
+        [InlineData(0, 350, new int[] { 0, 0, 3, 1, 0, 0, 0 }, 650, 1000)]
+
+        public void InputAmountAndPayReturnChangeAndBank(int expectedStatusCode, int expectedChange, int[] expectedBankCards, int amount, int pay)
         {
             var hackChangeMoney = new HackChangeMoney();
             var result = hackChangeMoney.CalculateChange(amount, pay);
             Assert.Equal(expectedChange, result.Change);
             Assert.Equal(expectedBankCards, result.BankCards);
-    
+            Assert.Equal(expectedStatusCode, result.StatusCode);
         }
     }
 }

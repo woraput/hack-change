48d58d64478127ccbd2e7dc5847e6b75d85ae6b3
woraput <woraput@kkumail.com>
2018-11-03 16:29:08 +0700

    add cors

--- HackChange.Api/Startup.cs
+++ HackChange.Api/Startup.cs
@@ -26,6 +26,8 @@ namespace HackChange.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +41,12 @@ namespace HackChange.Api
             {
                 app.UseHsts();
             }
+
+            app.UseCors(builder =>
+                builder.WithOrigins("*")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+            );
 
             app.UseHttpsRedirection();
             app.UseMvc();

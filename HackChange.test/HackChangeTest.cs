d98f1bfdb3c5325ee7aa7f31610e2b2cca027d1f
woraput <woraput@kkumail.com>
2018-11-03 14:56:28 +0700

    refactor test

--- HackChange.test/HackChangeTest.cs
+++ HackChange.test/HackChangeTest.cs
@@ -5,22 +5,15 @@ namespace HackChange.test
 {
     public class HackChangeTest
     {
-        [Fact]
-        public void InputAmountAndPayReturnChangeAndBank()
+        [Theory]
+        [InlineData(648, new int[7] { 0, 1, 1, 0, 2, 1, 3 }, 352, 1000)]
+        [InlineData(350, new int[7] { 0, 0, 3, 1, 0, 0, 0 }, 650, 1000)]
+        public void InputAmountAndPayReturnChangeAndBank(int expectedChange, int[] expectedBankCards, int amount, int pay)
         {
             var hackChangeMoney = new HackChangeMoney();
-            var result = hackChangeMoney.CalculateChange(650, 1000);
-            Assert.Equal(350, result.Change);
-            Assert.Equal(new int[7] { 0, 0, 3, 1, 0, 0, 0 }, result.BankCards);
-        }
-
-        [Fact]
-        public void InputAmountAndPayReturnChangeAndBank2()
-        {
-            var hackChangeMoney = new HackChangeMoney();
-            var result = hackChangeMoney.CalculateChange(352, 1000);
-            Assert.Equal(648, result.Change);
-            Assert.Equal(new int[7] { 0, 1, 1, 0, 2, 1, 3 }, result.BankCards);
+            var result = hackChangeMoney.CalculateChange(amount, pay);
+            Assert.Equal(expectedChange, result.Change);
+            Assert.Equal(expectedBankCards, result.BankCards);
         }
     }
 }

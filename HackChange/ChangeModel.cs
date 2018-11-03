d333bdc0dce4700aaafac58647a6a62f8571cc01
woraput <woraput@kkumail.com>
2018-11-03 15:08:35 +0700

    change model

--- HackChange/ChangeModel.cs
+++ HackChange/ChangeModel.cs
@@ -2,7 +2,20 @@ namespace HackChange
 {
     public class ChangeModel
     {
+        /// <summary>
+        ///  จำนวนเงินถอน
+        /// </summary>
         public int Change { get; set; }
+        /// <summary>
+        /// จำนวนแบ่งที่ถอน 1000 500 100 50 20 5 1
+        /// </summary>
         public int[] BankCards { get; set; }
+        /// <summary>
+        /// Example
+        /// 0 = สำเร็จ
+        /// 1 = ใส่ข้อมูลไม่ครบ
+        /// 2 = เงินจ่ายไม่พอ
+        /// </summary>
+        public int StatusCode { get; set; }
     }
 }

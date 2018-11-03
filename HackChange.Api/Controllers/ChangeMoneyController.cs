b52cbe5eea56194ab5410beeb0c4c50db951fb53
woraput <woraput@kkumail.com>
2018-11-03 16:24:43 +0700

    add api

---
+++ HackChange.Api/Controllers/ChangeMoneyController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HackChange.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChangeMoneyController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet("{amount}/{pay}")]
+        public ActionResult<ChangeModel> Get(int amount, int pay)
+        {
+            var hackChangeMoney = new HackChangeMoney();
+            var result = hackChangeMoney.CalculateChange(amount, pay);
+            return result;
+        }
+    }
+}
